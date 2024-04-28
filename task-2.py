b400f6aa74599e5da64df275d86dd204e5e3c2e3
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-28 16:20:17 +0300

    Feat(Files) Added task 2

---
+++ task-2.py
@@ -0,0 +1,19 @@
+def get_cats_info(path):
+    cats_list = []
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            for line in file:
+                parts = line.strip().split(",")
+                if len(parts) == 3:
+                    cat_info = {"id": parts[0], "name": parts[1], "age": parts[2]}
+                    cats_list.append(cat_info)
+                else:
+                    print(f"Помилка: Неправильний формат даних '{line.strip()}'")
+    except FileNotFoundError:
+        print("Помилка: Файл не знайдено.")
+    return cats_list
+
+
+# Приклад використання функції
+cats_info = get_cats_info("cats_file.txt")
+print(cats_info)
